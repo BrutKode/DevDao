f0d170c867e8c2718206442c22b96eac6b9ae178
BrutKode <usefulvloging@gmail.com>
2023-07-19 21:12:38 +0530

    Play wagmi

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useAccount, useSignMessage } from "wagmi";
 
 export const Home = () => {
+  const { address } = useAccount();
+  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+    message: "Welcome to developer dao captain, get ready to sail!",
+  });
   return (
-    <div className="card" style={{ "width": "18rem", "margin": "10px" }}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-        <a href="#" className="card-link">Card link</a>
-        <a href="#" className="card-link">Another link</a>
-      </div>
+    <div>
+      <p>Your Account: {address}</p>
+      <button disabled={isLoading} onClick={() => signMessage()}>
+        Sign message
+      </button>
+      {isSuccess && <div>Signature: {data}</div>}
+      {isError && <div>Error signing message</div>}
     </div>
   );
 };
